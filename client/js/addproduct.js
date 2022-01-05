8d94ec95f06a601798b9af323dcc507e3918eb1a
sugoke <sugoke@hotmail.com>
2022-01-05 15:37:48 +0100

    new

--- client/js/addproduct.js
+++ client/js/addproduct.js
@@ -6,7 +6,7 @@ Template.addproduct.events({
   var numItems = $('.underlyings').length
   numItems++
   console.log(numItems)
-  $(".underlyings_input").append('<tr id="underlying_line_' + numItems + '"><th scope="row"><label for=id="underlying_' + numItems + '"  class="mb-2">Ticker</label><div class="input-group"><input type="text" class="form-control underlyings underlyings_change" id=id="underlying_' + numItems + '" placeholder="Ticker" aria-label="Ticker" aria-describedby="basic-addon2"><div class="input-group-append"><select class="form-select underlyings_change" id="country_' + numItems + '" aria-label="Default select example"><option selected>US</option><option value="1">FR</option><option value="2">DE</option></select></div></div></th><td><label for="close_1"  class="mb-2">Close ({{td}})</label><input type="text" class="form-control cash" id="close_' + numItems + '" value="" disabled></td><td><label for="strike_1"  class="mb-2">Strike</label><input type="text" class="form-control cash" id="strike_' + numItems + '" value=""></td><td><label for="remove_underlying_' + numItems + ' "  class="mb-2">Del.</label><button class="btn btn-outline-theme form-control" type="button" id="remove_underlying_' + numItems + ' " onclick="$(this).parent().parent().remove();">X</button></td></tr>');
+  $(".underlyings_input").append('<tr id="underlying_line_' + numItems + '"><th scope="row"><label for=id="underlying_' + numItems + '"  class="mb-2">Ticker</label><div class="input-group"><input type="text" class="form-control underlyings underlyings_change" id=id="underlying_' + numItems + '" placeholder="Ticker" aria-label="Ticker" aria-describedby="basic-addon2"  autocomplete="off"><div class="input-group-append"><select class="form-select underlyings_change" id="country_' + numItems + '" aria-label="Default select example"><option selected>US</option><option value="1">FR</option><option value="2">DE</option></select></div></div></th><td><label for="close_1"  class="mb-2">Close ({{td}})</label><input type="text" class="form-control cash" id="close_' + numItems + '" value="" disabled></td><td><label for="strike_1"  class="mb-2">Strike</label><input type="text" class="form-control cash" id="strike_' + numItems + '" value=""  autocomplete="off"></td><td><label for="remove_underlying_' + numItems + ' "  class="mb-2">Del.</label><button class="btn btn-outline-theme form-control" type="button" id="remove_underlying_' + numItems + ' " onclick="$(this).parent().parent().remove();">X</button></td></tr>');
 
   //cash_mask_id('strike_' + numItems);
 
@@ -63,7 +63,15 @@ setTimeout(function() {
   var autocall_base_level = parseFloat(document.getElementById('autocall_barrier_input').value) / 100;
   var protection_barrier = Number(document.getElementById('protection_barrier_input').value);
   var stepdown = document.getElementById('step_down').checked;
+  var non_call = document.getElementById('step_down').checked;
   var periodicity = document.getElementById('periodicity').value;
+
+  if (stepdown) {
+    var stepdown_after = document.getElementById('stepdown_input').value;
+    var shift = parseFloat(document.getElementById('shift_input').value) / 100;
+
+    var start_sd = adjusted_date(date_start_sd(stepdown_after, trade_date))
+  }
 
     }, 50);
 
@@ -90,4 +98,12 @@ Template.addproduct.onRendered(function() {
 
 
 
+});
+
+Template.addproduct.helpers({
+  product_type: function(){
+
+    if (Router.current().params.query.type == "reverse_convertible") {return "Reverse Convertible"}
+
+  }
 });
